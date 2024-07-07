f15952f82a0838d0a59da959ec5563c79e5c4921
Shriram N <shri64king@gmail.com>
2024-07-07 22:13:39 +0530

    Remove unused DB

--- api/src/main/resources/queries/local_queries_ddl.sql
+++ api/src/main/resources/queries/local_queries_ddl.sql
@@ -1,13 +1,3 @@
-create schema if not exists users charset utf8mb4 collate utf8mb4_unicode_ci;
-DROP table if exists users.app_users;
-CREATE TABLE users.app_users (
-    user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    user_name VARCHAR(100) UNIQUE KEY,
-    `password` VARCHAR(100),
-    email_id VARCHAR(400)
-)  CHARSET=UTF8MB4 , COLLATE = UTF8MB4_UNICODE_CI;
-
-
 create schema if not exists accounts charset utf8mb4 collate utf8mb4_unicode_ci;
 drop table if exists accounts.`account_details`;
 CREATE TABLE IF NOT EXISTS accounts.`account_details` (
